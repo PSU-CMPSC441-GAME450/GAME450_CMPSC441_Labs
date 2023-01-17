114ad80919c78f90f8e6d9dd920a3fe869a50ff4
Marcus James <mcj5243@psu.edu>
2023-01-17 11:12:07 -0500

    new name

--- src/lab1/git_intro.py
+++ src/lab1/git_intro.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-print("Hello Marcus")
-=======
 '''Lab 1.1: Git Intro
 
 In this lab, you will learn how to use git and github to manage your code.
@@ -13,4 +10,4 @@ Commit your changes to your local repository.
 Push your changes to your forked repository on github.
 '''
 print("hello")
->>>>>>> 1d00b3cf3f0adc55131dbe735e3c6e77dc2c9d7f
+
