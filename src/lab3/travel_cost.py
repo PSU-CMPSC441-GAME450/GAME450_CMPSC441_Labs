db5e3773993ddb737b89b0fdf3056d066f637da1
Marcus James <marcus.james19@yahoo.com>
2023-01-27 19:26:12 -0500

    added code

--- src/lab3/travel_cost.py
+++ src/lab3/travel_cost.py
@@ -35,35 +35,14 @@ def get_route_cost(route_coordinate, game_map):
     """
     # Build a path from start to end that looks like [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 4)]
     pass 
-    start, end = route_coordinate
-    x1, y1 = start
-    x2, y2 = end
-    steep = abs(y2 - y1) > abs(x2 - x1)
-    if steep:
-      x1, y1 = y1, x1
-      x2, y2 = y2, x2
-    if x1 > x2:
-      x1, x2 = x2, x1
-      y1, y2 = y2, y1
-    deltax = x2 - x1
-    deltay = abs(y2 - y1)
-    error = deltax / 2
-    y = y1
-    ystep = None
-    if y1 < y2:
-      ystep = 1
-    else:
-      ystep = -1
-    path = []
-    for x in range(x1, x2 + 1):
-      if steep:
-        path.append((y, x))
-      else:
-        path.append((x, y))
-        error -= deltay
-    if error < 0:
-      y += ystep
-      error += deltax
+    width = max(game_map.shape[0],game_map.shape[1])
+
+    grid = Grid(width=width, height=width)
+
+    start = grid.node(route_coordinate[0][0], route_coordinate[0][1])
+    end = grid.node(route_coordinate[1][0], route_coordinate[1][1])
+    finder = AStarFinder(diagonal_movement = DiagonalMovement.always)
+    path, pathcost  = finder.find_path(start, end, grid)
     return game_map[tuple(zip(*path))].sum()
     
 
