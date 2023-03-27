330567103c1ef5c4b900014ed9d9da68364e497b
Emily Green <exg941@psu.edu>
2023-03-27 01:33:23 -0400

    lab11 pt2

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -1,12 +1,23 @@
+import pygame
+from lab11.turn_combat import CombatPlayer
+import random
 """ Create PyGameAIPlayer class here"""
 
 
 class PyGameAIPlayer:
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def selectAction(self, state):
+        return ord(str(random.randrange(10)))
 
 
 """ Create PyGameAICombatPlayer class here"""
 
 
-class PyGameAICombatPlayer:
-    pass
+class PyGameAICombatPlayer(CombatPlayer):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def weapon_selecting_strategy(self):
+        self.weapon = random.randrange(2)
