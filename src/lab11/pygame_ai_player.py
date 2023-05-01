1f501eefe50478e1eb2a8a2ff00f861d37314fa4
MarcusJames0930 <marcus.james19@yahoo.com>
2023-05-01 18:49:40 -0400

    added restrcited routes

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -8,63 +8,71 @@ class PyGameAIPlayer:
         self.path = []
         self.path_index = 0
 
+class PyGameAIPlayer:
+    def __init__(self):
+            self.path = []
+            self.path_index = 0
+ 
     def selectAction(self, state):
+        current_city = state.current_city
+        destination_city = state.destination_city
+        routes = state.routes
+        
+        # Find the routes that start from the current city
+        valid_routes = [i for i, r in enumerate(routes) if r[0] == current_city]
+        
+        # If there are no valid routes, return a random action as before
+        if not valid_routes:
+            return random.randint(48, 57)
+        
+        # Find the neighboring cities that the player can travel to
+        neighbors = [r[1] for i, r in enumerate(routes) if i in valid_routes]
+        
+        # If the player is already traveling, return the current destination
         if state.travelling:
-            return ord(" ")
-        elif state.current_city == state.destination_city:
-            valid_cities = []
-            for r in state.routes:
-                if r[0] == state.current_city:
-                    valid_cities.append(r[1])
-                elif r[1] == state.current_city:
-                    valid_cities.append(r[0])
-            if valid_cities:
-                destination = random.choice(valid_cities)
-                self.path = self.find_path(state.current_city, destination, state.routes)
-                self.path_index = 0
-                return ord(str(destination))
-            else:
-                return ord(" ")
-        elif self.path_index < len(self.path):
-            next_city = self.path[self.path_index]
-            self.path_index += 1
-            return ord(str(next_city))
-        else:
-            state.destination_city = state.current_city
-            return ord(" ")
-
-    def find_path(self, start, end, routes):
-        graph = defaultdict(list)
-        for r in routes:
-            graph[r[0]].append(r[1])
-            graph[r[1]].append(r[0])
-
-        q = deque()
-        visited = [False] * len(graph)
-        path = [-1] * len(graph)
-
-        q.append(start)
-        visited[start] = True
-
-        while q:
-            u = q.popleft()
-            for v in graph[u]:
-                if not visited[v]:
-                    visited[v] = True
-                    path[v] = u
-                    q.append(v)
-
-        p = []
-        if path[end] != -1:
-            while end != -1:
-                p.insert(0, end)
-                end = path[end]
-        return p
-
-
-
-
-
+            return str(destination_city).encode()[0]
+        
+        # Otherwise, select a random neighbor as the destination city
+        destination_city = random.choice(neighbors)
+        route_index = valid_routes[neighbors.index(destination_city)]
+        
+        # Return the index of the selected route as the action
+        return str(route_index).encode()[0]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         #return random.randint(48, 57) # ASCII values for digits 0-9
 
 class PyGameAICombatPlayer(CombatPlayer):
