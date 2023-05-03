1aa85eb6953e4854963a921b29b99f05266bf936
MarcusJames0930 <marcus.james19@yahoo.com>
2023-05-03 08:16:48 -0400

    journaling

--- src/lab11/journaling.py
+++ src/lab11/journaling.py
@@ -1,13 +1,14 @@
 import torch
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import transformers
 
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = GPT2LMHeadModel.from_pretrained('gpt2')
+tokenizer = transformers.GPT2Tokenizer.from_pretrained('gpt2')
+model = transformers.GPT2LMHeadModel.from_pretrained('gpt2')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model.to(device)
 
 def generate_journal_entry(city_name):
-    prompt = f"I arrived in {city_name} today. "
+    prompt = "I arrived in " + city_name + " today. "
     input_ids = tokenizer.encode(prompt, return_tensors='pt').to(device)
-    output = model.generate(input_ids=input_ids, max_length=200, do_sample=True)
+    output = model.generate(input_ids, max_length=200, do_sample=True)
     journal_entry = tokenizer.decode(output[0], skip_special_tokens=True)
     return journal_entry
